fb5dd24342988d77ae4404b4332c58c6b92fadb0
m3xy <myip6373@gmail.com>
2023-04-04 19:36:56 +0100

    pairing

--- IllustratorComponent/ConfirmServices.java
+++ IllustratorComponent/ConfirmServices.java
@@ -1,12 +1,15 @@
 package IllustratorComponent;
 
+import UserComponent.User;
+
 public class ConfirmServices {
 
     int cost;
     boolean contract;
     int numberOfImages;
+    User user;
 
-    public boolean ServiceToBeProvided(String description){
+    public boolean ServiceToBeProvided(User user, String description){
 
         //true if agrees
         //false if disagrees
