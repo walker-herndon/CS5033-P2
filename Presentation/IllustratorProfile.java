814f296213980a6c6384714c4ce94baefb5c9f5a
ojdalrymple <oscardalrymple@rocketmail.com>
2023-04-06 17:51:23 +0100

    create illustrator profile

---
+++ Presentation/IllustratorProfile.java
@@ -0,0 +1,30 @@
+package Presentation;
+
+import javax.swing.*;
+
+public class IllustratorProfile extends JFrame {
+
+    public IllustratorProfile() {
+        JComponent navigation = new JComponent() {
+            // navigation logic
+        };
+
+        JButton logout = new JButton();
+
+        JComponent illustratorInformation = new JComponent() {
+            // illustrator information pulled from UserManager via API call
+        };
+
+
+        JComponent rating = new JComponent() {
+          
+        };
+
+     
+        if(user clicks on profile){
+          setVisible(true);
+        }  
+
+    }
+
+}
