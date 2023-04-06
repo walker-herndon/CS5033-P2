8fff173978dca74c26fc1d743bc5a0809f082b9b
ojdalrymple <oscardalrymple@rocketmail.com>
2023-04-06 17:48:58 +0100

    Create Illustrator display

---
+++ Presentation/IllustratorDisplay.java
@@ -0,0 +1,19 @@
+package Presentation;
+import javax.swing.*;
+
+
+public class IllustratorDisplay extends JFrame {
+    // display window for illustrators
+    public IllustratorDisplay() {
+        JComponent navigation = new JComponent() {
+            // navigation logic
+        };
+        
+        JPanel illustratorList = new JPanel();
+        
+       
+        if(on illustrator page){
+          setVisible(true);
+        }
+    }
+}
